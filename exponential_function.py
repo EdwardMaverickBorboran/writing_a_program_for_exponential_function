3eff17423a4daaffba06d0fce5fa76ba36e23bb3
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-23 19:14:34 +0800

    Added an integer user input.

---
+++ exponential_function.py
@@ -0,0 +1,18 @@
+# Create a program that will --
+
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+# Note here exp is a non-negative integer, and the base is an integer.
+
+# Expected results: 
+# base = 2
+# exponent = 5
+
+# 2 raises to the power of 5: 32 i.e. (2 *2 * 2 *2 *2 = 32)
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Asking the user to input a number
+user_base = int(input("Enter number for base:"))
+user_exp = int(input("Enter number for base:"))
