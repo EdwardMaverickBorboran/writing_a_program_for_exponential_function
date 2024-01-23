d263b363474dce22f8df2c5d97f4801f56e479ce
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-23 19:32:15 +0800

    I used the ** operator since it functions for exponentiation. Final code.

--- exponential_function.py
+++ exponential_function.py
@@ -13,6 +13,19 @@
 
 # pseudocode
 
+def exp(user_base, user_exp):
+
+    base = user_base
+    exponent = user_exp
+
+    final_results = base ** exponent
+
+    print(base, "raises to the power of", exponent,":", final_results)
+
+
 # Asking the user to input a number
 user_base = int(input("Enter number for base:"))
-user_exp = int(input("Enter number for base:"))
+user_exp = int(input("Enter number for exp:"))
+
+
+exp(user_base, user_exp)
